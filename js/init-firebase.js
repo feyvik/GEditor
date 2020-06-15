f98d1207af8e24250843a325b6872b222e5e6988
Kelvin Gobo <gobokel@gmail.com>
2020-06-15 10:38:47 +0100

    fix(boilerplate): add firebase configuration

--- js/init-firebase.js
+++ js/init-firebase.js
@@ -1,7 +1,13 @@
 /* eslint-disable no-undef */
 // add firebase project configuration here
 const config = {
-
+	apiKey: 'AIzaSyAotiw6-oVs_khbR8wcZjun_jp4Ega2I3k',
+	authDomain: 'docs-clone.firebaseapp.com',
+	databaseURL: 'https://docs-clone.firebaseio.com',
+	projectId: 'docs-clone',
+	storageBucket: 'docs-clone.appspot.com',
+	messagingSenderId: '849864268762',
+	appId: '1:849864268762:web:4390029e600da9c64ffee3'
 };
 
 firebase.initializeApp(config);
