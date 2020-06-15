e5a0a6a2eb6b8c32b27b010e22ab327b998b82d6
favour vivian woka <vivianemma97@gmail.com>
2020-06-15 21:27:10 +0100

    feat(sign-up): Implement google sign authentication

--- js/auth.js
+++ js/auth.js
@@ -4,9 +4,32 @@ function signUpWithEmailAndPassword(email, password, fullname) {}
 function signInWithEmailAndPassword(email, password) {}
 
 function authenticateWithGoogle() {
-  var provider = new firebase.auth.GoogleAuthProvider();
-  firebase.auth().signInWithPopup(provider);
+  const provider = new firebase.auth.GoogleAuthProvider();
+  // firebase.auth().signInWithPopup(provider);
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then(function (result) {
+      const userDetails = {
+        token: result.credential.accessToken,
+        user: result.user.displayName,
+        photo: result.user.photoURL,
+        uid: result.user.uid,
+      };
+      console.log(userDetails);
+      window.location.replace("./profile.html");
+      console.log(result.user);
+    })
+    .catch(function (error) {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      const email = error.email;
+      const credential = error.credential;
+      console.log(errorCode, errorMessage, email, credential);
+    });
 }
 
-var signInButtonElement = document.getElementById("sign-in")
-signInButtonElement.addEventListener('click', authenticateWithGoogle)
+const signUpButtonElement = document.getElementById("signup");
+signUpButtonElement.addEventListener("click", signUpWithEmailAndPassword);
+const signInGoogleButton = document.getElementById("sign-in");
+signInGoogleButton.addEventListener("click", authenticateWithGoogle);
